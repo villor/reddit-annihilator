8790330b6d7a171d7a6f5f0220cd6c70c4d5497f
Viktor Gustavsson <villor94@gmail.com>
2016-05-19 00:41:47 +0200

    Add regex check to allow some reddit urls

--- annihilator.js
+++ annihilator.js
@@ -1,3 +1,7 @@
-document.open();
-document.write('nope');
-document.close();
+// If the url is for the front page or a specific subreddit (or /r/all), the page is replaced.
+// If the url contains /comments after the /r/{something} part it is not replaced (to allow specific reddit posts to be loaded)
+if (/^.*:\/\/.*reddit\.com(\/)?(r\/((?!\/comments).)*)?$/.test(window.location.href)) {
+	document.open();
+	document.write('nope');
+	document.close();
+}
